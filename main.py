3bf46cb4dda97fe4aac0c5cba60ec37b1da97e27
James Klimavicz <42780890+jsklimavicz@users.noreply.github.com>
2024-08-15 21:16:51 -0400

    code cleanup

--- main.py
+++ main.py
@@ -7,18 +7,25 @@ from chromatogram.chromatogram import Baseline
 from pydash import get as _get
 from injection import Injection
 from system import *
+import pandas as pd
 
 import random
 
 random.seed(10)
+
+
+from cProfile import Profile
+from pstats import SortKey, Stats
+
+# with Profile() as profile:
 
 sample_dict = {
     "sample_name": "test-1",
     "location": "R:A1",
     "compound_list": "cystine, guanosine, chloroquine, DPU, coumarin",
     "concentration_list": "0.5, 2.1, 3.2, 1.3, 2.4",
-    "num_random_peaks": 5,
-    "max_random_concentration": 0.5,
+    "num_random_peaks": 10,
+    "max_random_concentration": 1,
 }
 
 sample_dict["compound_list"] = [
@@ -40,14 +47,14 @@ system = System(name="James Test", column=column)
 my_sample = Sample(**sample_dict)
 # my_sample.print_compound_list()
 
-
 with open("./methods/instrument_methods.json") as f:
     method_list = json.load(f)
 
-method_1 = Method(**_get(method_list, "0"))
-method_2 = Method(**_get(method_list, "1"))
-method_3 = Method(**_get(method_list, "2"))
-method_4 = Method(**_get(method_list, "3"))
+# method_1 = Method(**_get(method_list, "0"))
+method_1 = Method(**_get(method_list, "5"))
+# method_2 = Method(**_get(method_list, "1"))
+# method_3 = Method(**_get(method_list, "2"))
+# method_4 = Method(**_get(method_list, "3"))
 
 injection1 = Injection(sample=my_sample, method=method_1, system=system)
 # injection2 = Injection(sample=my_sample, method=method_2, system=system)
@@ -55,8 +62,8 @@ injection1 = Injection(sample=my_sample, method=method_1, system=system)
 # injection4 = Injection(sample=my_sample, method=method_4, system=system)
 
 injection1.plot_chromatogram("UV_VIS_1", c="red")
-data = injection1.get_chromatogram_data("UV_VIS_1")
-print(data.head())
+data: pd.DataFrame = injection1.get_chromatogram_data("UV_VIS_1")
+data.to_parquet("./data_processing/test.parquet")
 # injection2.plot_chromatogram("UV_VIS_1", h_offset=0, c="blue")
 # injection3.plot_chromatogram("UV_VIS_1", h_offset=0, c="black")
 # injection4.plot_chromatogram("UV_VIS_1", h_offset=0, c="green")
@@ -67,3 +74,6 @@ print(data.head())
 # injection.plot_chromatogram("UV_VIS_4", h_offset=0.9, v_offset=90, c="green")
 
 plt.show()
+
+# f = open("stats.prof", "a")
+# Stats(profile, stream=f).strip_dirs().sort_stats("cumtime").print_stats()
