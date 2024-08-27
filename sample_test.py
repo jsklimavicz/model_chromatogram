bf0d8503310c68c0e010a7477cfd1983704b9dc8
James Klimavicz <42780890+jsklimavicz@users.noreply.github.com>
2024-08-27 13:58:29 -0400

    add method to produce samples for degradation reactions

--- sample_test.py
+++ sample_test.py
@@ -1,37 +1,46 @@
 from samples import Sample, SampleCreator
+import numpy as np
 
 
 sample_creator = SampleCreator()
 
-samples = sample_creator.product_stability_samples(
-    n_samples=10,
-    timing_interval=10,
-    compound_mapping=[
-        {
-            "intial_compound": "90094-11-4",
-            "compounds": ["56843-54-0", "50-48-6", "117-89-5", "72-43-5", "79-11-8"],
-            "k": [10, 10, 2, 2, 3],
-            "initial_concentration": 10,
-        },
-        {
-            "intial_compound": "56843-54-0",
-            "compounds": ["56-54-2", "1617-90-9"],
-            "k": [1, 3],
-        },
-        {
-            "intial_compound": "50-48-6",
-            "compounds": ["56-54-2", "93-76-5"],
-            "k": [1, 1],
-        },
-        {
-            "intial_compound": "117-89-5",
-            "compounds": ["773-76-2", "72-43-5"],
-            "k": [2, 3],
-        },
-        {
-            "intial_compound": "79-11-8",
-            "compounds": ["56-54-2", "93-76-5"],
-            "k": [1, 1],
-        },
-    ],
+compound_mapping = [
+    {
+        "initial_compound": "90094-11-4",
+        "compounds": [
+            ["56843-54-0", "50-48-6"],
+            ["117-89-5", "72-43-5"],
+            "79-11-8",
+        ],
+        "k": [0.00100, 0.002, 0.0032],
+        "initial_concentration": 10,
+    },
+    {
+        "initial_compound": "56843-54-0",
+        "compounds": [["56-54-2", "1617-90-9"]],
+        "k": [0.0072],
+    },
+    {
+        "initial_compound": "50-48-6",
+        "compounds": ["56-54-2", "93-76-5", "79-11-8"],
+        "k": [0.01, 0.04, 0.002],
+    },
+    {
+        "initial_compound": "117-89-5",
+        "compounds": ["773-76-2", "72-43-5"],
+        "k": [0.0006, 0.0003],
+    },
+    {
+        "initial_compound": "79-11-8",
+        "compounds": ["56-54-2"],
+        "k": [0.01],
+    },
+]
+
+
+sample_creator.product_stability_samples(
+    time_points=np.array(
+        [0, 1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 45, 60, 75, 90, 105, 120, 150, 180]
+    ),
+    compound_mapping=compound_mapping,
 )
--- samples/sample_creator.py
+++ samples/sample_creator.py
@@ -1,6 +1,9 @@
 from samples import Sample
 from compounds import Compound, COMPOUND_LIBRARY
 
+import numpy as np
+from scipy.linalg import eig, inv
+
 
 class SampleCreator:
     def __init__(self) -> None:
@@ -30,13 +33,71 @@ class SampleCreator:
     ) -> list[Sample]:
         pass
 
+    def _create_reaction_matrix(self, compound_mapping, compound_names):
+        n = len(compound_names)
+        A = np.zeros((n, n))
+
+        for reaction in compound_mapping:
+            initial_idx = compound_names.index(reaction["initial_compound"])
+
+            for i, products in enumerate(reaction["compounds"]):
+                rate = reaction["k"][i]
+                # Subtract from the initial compound
+                A[initial_idx, initial_idx] -= rate
+
+                # Add to the products
+                if isinstance(products, list):
+                    for product in products:
+                        product_idx = compound_names.index(product)
+                        A[product_idx, initial_idx] += rate
+                else:
+                    product_idx = compound_names.index(products)
+                    A[product_idx, initial_idx] += rate
+
+        return A
+
+    def _solve_reaction_ODE(self, t_values, A, initial_concentrations):
+        # Diagonalize the matrix A
+        eigvals, eigvecs = eig(A)
+
+        # Compute the matrix exponential for each time step
+        C_t = []
+        for t in t_values:
+            exp_diag = np.diag(np.exp(eigvals * t))
+            C_t.append(
+                np.dot(
+                    eigvecs,
+                    np.dot(exp_diag, np.dot(inv(eigvecs), initial_concentrations)),
+                )
+            )
+
+        return np.real(np.array(C_t))
+
     def product_stability_samples(
-        self, n_samples, timing_interval, compound_mapping
+        self, time_points: np.array, compound_mapping: list[dict]
     ) -> list[Sample]:
-        compound_id_set = set()
-        for item in compound_mapping:
-            compound_id_set.add(item["intial_compound"])
-            assert len(item["compounds"]) == len(item["k"])
-            for id in item["compounds"]:
-                compound_id_set.add(id)
-        print(compound_id_set)
+        # Get the compound names and initial concentrations
+        compound_names = set()
+        for reaction in compound_mapping:
+            compound_names.add(reaction["initial_compound"])
+            for products in reaction["compounds"]:
+                if isinstance(products, list):
+                    compound_names.update(products)
+                else:
+                    compound_names.add(products)
+        compound_names = list(compound_names)
+
+        initial_concentrations = np.zeros(len(compound_names))
+        for reaction in compound_mapping:
+            if "initial_concentration" in reaction.keys():
+                initial_idx = compound_names.index(reaction["initial_compound"])
+                initial_concentrations[initial_idx] = reaction["initial_concentration"]
+
+        # Construct the matrix A
+        A = self._create_reaction_matrix(compound_mapping, compound_names)
+
+        # Solve the system using linear algebra
+        concentrations_over_time = self._solve_reaction_ODE(
+            time_points, A, initial_concentrations
+        )
+        print(concentrations_over_time)
